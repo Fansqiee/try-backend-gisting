7bb0054530253a4503bf4f7667e3bcd93435ea34
Fansqiee <143506406+Fansqiee@users.noreply.github.com>
2023-09-03 02:12:54 +0700

    Add files via upload

--- test_db.js
+++ test_db.js
@@ -1,61 +1,82 @@
-const mqtt = require('mqtt');
-const { Client } = require('pg');
+const Mqtt = require("mqtt");
+const { Pool, Client } = require('pg');
 
-// MQTT settings
-const MQTT_BROKER = 'https://vps.isi-net.org';
-const MQTT_TOPIC = 'dummyICS';
-
-// Database settings
-const DB_CONFIG = {
+// Konfigurasi database PostgreSQL
+const dbConfig = {
   user: 'postgres',
   host: 'localhost',
   database: 'Local_DB',
   password: 'user',
-  port: 5432, // default PostgreSQL port
+  port: 5432 // Port default PostgreSQL
 };
 
-// Create MQTT client
-const mqttClient = mqtt.connect(MQTT_BROKER);
+const pool = new Pool(dbConfig);
 
-// Setup PostgreSQL client
-const pgClient = new Client(DB_CONFIG);
-pgClient.connect();
-
-// MQTT message handling
-mqttClient.on('connect', () => {
-  console.log('Connected to MQTT broker');
-  mqttClient.subscribe(MQTT_TOPIC);
+const mqtt = Mqtt.connect("mqtt://vps.isi-net.org", {
+    port: 1883,
+    username: "unila",
+    password: "pwdMQTT@123",
+    clientId: 'mqttjs_' + Math.random(),
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    clean: true,
 });
 
-mqttClient.on('message', (topic, message) => {
-  try {
-    const sensorData = JSON.parse(message.toString());
-    const { datetime, humidity_280, pressure_280, temperature_280, temperature_388, pressure_388, phsensor, tdsSensor, moistureSensor, anemoMeter, windVane, currentSensor, rainIntensity, rainStatus } = sensorData;
+mqtt.on("connect", () => {
+    console.log(`MQTT Connected on host: mqtt://vps.isi-net.org`);
 
-    const insertQuery = `INSERT INTO sensor_data (datetime, humidity_280, pressure_280, temperature_280, temperature_388, pressure_388, phsensor, tdsSensor, moistureSensor, anemoMeter, windVane, currentSensor, rainIntensity, rainStatus) 
-                         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`;
-
-    const values = [datetime, humidity_280, pressure_280, temperature_280, temperature_388, pressure_388, phsensor, tdsSensor, moistureSensor, anemoMeter, windVane, currentSensor, rainIntensity, rainStatus];
-
-    pgClient.query(insertQuery, values, (error, results) => {
-      if (error) {
-        console.error('Error inserting data:', error);
-      } else {
-        console.log('Data inserted successfully');
-      }
+    const topic = "dummyICS";
+    mqtt.subscribe(topic, () => {
+        console.log("Subscribed to topic " + topic);
     });
-  } catch (error) {
-    console.error('Error processing MQTT message:', error);
-  }
-});
 
-mqttClient.on('error', (error) => {
-  console.error('MQTT error:', error);
-});
+    mqtt.on("message", (topic, message) => {
+        const payload = message.toString();
+        try {
+            const data = JSON.parse(payload);
 
-// Graceful shutdown
-process.on('SIGINT', () => {
-  mqttClient.end();
-  pgClient.end();
-  process.exit();
+            const datetime = data.datetime;
+            const humidity_280 = data.humidity_280;
+            const pressure_280 = data.pressure_280;
+            const temperature_280 = data.temperature_280;
+            const temperature_388 = data.temperature_388;
+            const pressure_388 = data.pressure_388;
+            const phsensor = data.phsensor;
+            const tdsSensor = data.tdsSensor;
+            const moistureSensor = data.moistureSensor;
+            const anemoMeter = data.anemoMeter;
+            const windVane = data.windVane;
+            const currentSensor = data.currentSensor;
+            const rainIntensity = data.rainIntensity;
+            const rainStatus = data.rainStatus;
+
+            console.log(`datetime = ${datetime}`);
+            console.log(`Humidity Sensor 280 = ${humidity_280}`);
+            console.log(`Pressure Sensor 280 = ${pressure_280}`);
+            console.log(`Temperature Sensor 280 = ${temperature_280}`);
+            console.log(`Temperature Sensor 380 = ${temperature_388}`);
+            console.log(`Pressure Sensor 388 = ${pressure_388}`);
+            console.log(`PH Sensor = ${phsensor}`);
+            console.log(`TDS Sensor = ${tdsSensor}`);
+            console.log(`Soil Moisture Sensor = ${moistureSensor}`);
+            console.log(`Anemometer Sensor = ${anemoMeter}`);
+            console.log(`Wind Direction = ${windVane}`);
+            console.log(`Current Sensor  = ${currentSensor}`);
+            console.log(`Rain Intensity = ${rainIntensity}`);
+            console.log(`Main Status = ${rainStatus}`);
+
+            const dataArray = [datetime, humidity_280, pressure_280, temperature_280, temperature_388, pressure_388, phsensor, tdsSensor, moistureSensor, anemoMeter, windVane, currentSensor, rainIntensity, rainStatus];
+            const insertQuery = `INSERT INTO sensor_data (datetime, humidity_280, pressure_280, temperature_280, temperature_388, pressure_388, phsensor, tdsSensor, moistureSensor, anemoMeter, windVane, currentSensor, rainIntensity, rainStatus) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)`;
+
+            pool.query(insertQuery, dataArray, (err, res) => {
+                if (err) {
+                    console.error('Error inserting data into the database:', err);
+                } else {
+                    console.log(`DATA INSERTED TO DATABASE: Time - ${datetime}, HMDTY 280 - ${humidity_280}, PRSS 280 - ${pressure_280}, TEMP 280 - ${temperature_280}, TEMP 388 - ${temperature_388}, PRSS 388 - ${pressure_388}, PH - ${phsensor}, TDS - ${tdsSensor}, MOIS - ${moistureSensor}, AMeter - ${anemoMeter}, WVane - ${windVane}, CRNT - ${currentSensor}, RIntensity - ${rainIntensity}, RStatus - ${rainStatus}`);
+                }
+            });
+        } catch (e) {
+            console.error('Error parsing JSON:', e);
+        }
+    });
 });
